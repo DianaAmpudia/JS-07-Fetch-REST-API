0631b8e2e05ca63e2210530c87c0a046e1178e49
Diana Ampudia <dianalauraampudia@gmail.com>
2024-08-09 08:28:14 -0600

    create pokemon card and loadInitialPokemon function

--- fetch.js
+++ fetch.js
@@ -1,29 +1,67 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
-// Fetch no async
-/*
-fetch(BASE_URL + 'pokemon/ditto')
-    .then(res => res.json())
-    .then(data => console.log(data));
-*/
-// fetch async
-
 const fetchPokemon = async (pokemon) => {
   try {
     const response = await fetch(`${BASE_URL}pokemon/${pokemon}`);
-    const parsedResponse = await response.json();
-    return parsedResponse;
+    //console.log(response);
+    const parsedData = await response.json();
+    console.log(parsedData);
+    return parsedData;
   } catch (err) {
     console.error(err);
   }
 };
 
-// get button
+// Function to create a card for each pokemon.
+function createPokemonCard(pokemon) {
+  // Get the container of the card
+  const container = document.getElementById("card-container");
+  container.innerHTML = ""; // Clear previous content
+
+  // Create the card element
+  const card = document.createElement("div");
+  card.classList.add("pokemon-card");
+
+  // Create image element
+  const imageElement = document.createElement("img");
+  imageElement.src = pokemon.sprites.front_default;
+  imageElement.alt = `${pokemon.name} Image`;
+  imageElement.classList.add("pokemon-image");
+
+  // Add the image to the card
+  card.appendChild(imageElement);
+
+  // Create pokemon id element
+  const pokemonId = document.createElement("h1");
+  pokemonId.textContent = `#${pokemon.id}`;
+  pokemonId.classList.add("card-pokemon-id");
+
+  // Create pokemon name element
+  const pokemonName = document.createElement("h2");
+  pokemonName.textContent = pokemon.name;
+  pokemonName.classList.add("card-pokemon-name");
+
+  // Create pokemon weight element
+  const pokemonWeight = document.createElement("p");
+  //Acording to the pokeapi, the weight of the pokemons are in hectograms.
+  pokemonWeight.textContent = `Weight: ${pokemon.weight} hectograms`;
+  pokemonWeight.classList.add("card-pokemon-weight");
+
+  // Append elements to the card
+  card.append(pokemonId, pokemonName, pokemonWeight);
+
+  // Append the card to the container
+  container.appendChild(card);
+}
+
+// Get Button
 document.getElementById("get-btn").addEventListener("click", async () => {
   const text = document.getElementById("poke-name").value.toLowerCase();
   const pokemon = await fetchPokemon(text);
-  localStorage.setItem("currentPokeId", pokemon.id);
-  console.log(pokemon.name);
+  if (pokemon) {
+    localStorage.setItem("currentPokemonId", pokemon.id.toString());
+    createPokemonCard(pokemon);
+  }
 });
 
 /* ==== Buttons ==== 
@@ -36,18 +74,56 @@ document.getElementById("get-btn").addEventListener("click", async () => {
 
 // Prev Button
 document.getElementById("previous-btn").addEventListener("click", async () => {
-  let currentPokemonId = parseInt(localStorage.getItem("currentPokemonId"));
-  const newId = Math.max(1, currentPokemonId - 1);
+  let currentPokemonId =
+    parseInt(localStorage.getItem("currentPokemonId"));
+  const newId = Math.max(currentPokemonId - 1, 1);
   const pokemon = await fetchPokemon(newId);
   localStorage.setItem("currentPokemonId", newId);
-  console.log(pokemon.id, "Prev Button");
+  createPokemonCard(pokemon);
 });
 
 // Next Button
 document.getElementById("next-btn").addEventListener("click", async () => {
-  let currentPokemonId = parseInt(localStorage.getItem("currentPokemonId"));
+  let currentPokemonId =
+    parseInt(localStorage.getItem("currentPokemonId")) || 1;
   const newId = currentPokemonId + 1;
   const pokemon = await fetchPokemon(newId);
+
   localStorage.setItem("currentPokemonId", newId);
-  console.log(pokemon.id, "Next Button");
+  console.log("Set new Pokemon ID:", newId);
+  createPokemonCard(pokemon);
 });
+
+/* 
+===== Asynchronous function to load the initial pokemon ======
+ - First we get the current id from localStorage.
+   --The getItem() method returns value of the specified Storage Object item.
+ - Then we check if there's a saved id in local storage. If yes, we fetch the pokemon and create the card.
+ - If there's no saved pokemon, it loads the first one.
+ */
+async function loadInitialPokemon() {
+  const savedPokemonId = localStorage.getItem("currentPokemonId");
+  console.log(savedPokemonId);
+
+  if (savedPokemonId) {
+    const pokemon = await fetchPokemon(savedPokemonId);
+    if (pokemon) {
+      createPokemonCard(pokemon);
+    }
+  } else {
+    const pokemon = await fetchPokemon(1);
+    if (pokemon) {
+      localStorage.setItem("currentPokemonId", "1");
+      createPokemonCard(pokemon);
+    }
+  }
+}
+
+// Event listener -- DOMContentLoaded is an event that allows us to know when all the DOM elements(the HTML elements of a project) are loaded.
+document.addEventListener("DOMContentLoaded", loadInitialPokemon);
+
+/*
+NOTES:
+getItem() method : localStorage.getItem(keyname)
+The keyname is a string specifying the name of the key we want to get the value of.
+*/
